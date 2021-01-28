45e587355e82bc15f6ed8e0c52a1c23b7297d850
Deiklov <romanov408g@mail.ru>
2021-01-28 09:24:51 +0300

    base webpack jsx config

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+
+module.exports = (env, options) => {
+    return {
+        entry: path.join(__dirname, "src", "index.js"),
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'bundle.js',
+        },
+
+        // devtool: 'cheap-eval-source-map',
+
+        module: {
+            rules: [
+                {
+                    test: /\.jsx$|\.es6$|\.js$/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ["@babel/preset-env", "@babel/preset-react"],
+                        }
+                    },
+                    exclude: /(node_modules|bower_components)/
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                importLoaders: 1,
+                                minimize: (options.mode == 'production' ? true : false),
+                                sourceMap: true,
+                            }
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                plugins: [require('autoprefixer')]
+                            }
+                        },
+                    ],
+                },
+                {
+                    test: /\.(png|jpg|gif)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                name: '[name].[ext]',
+                                outputPath: 'images/'
+                            }
+                        }
+                    ]
+                },
+            ],
+        },
+
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: 'style.css',
+                chunkFilename: '[id].css'
+            }),
+            new HtmlWebpackPlugin({
+                template: path.join(__dirname, 'src', 'index.html'),
+                filename: 'index.html',
+            })
+        ],
+
+    }
+};
