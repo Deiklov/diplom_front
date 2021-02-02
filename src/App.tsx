072eb236f335717ec7fe4e659a2e38f728f5f4f7
Deiklov <romanov408g@mail.ru>
2021-02-02 23:00:43 +0300

    base routing

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,35 @@ import AuthViewModelImpl from './presentation/view-model/auth/AuthViewModelImpl'
 import AuthFakeApi from './data/auth/AuthFakeApi';
 import LoginUseCase from './domain/interactors/auth/LoginUseCase';
 import AuthHolder from './domain/entity/auth/models/AuthHolder';
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
 
 function App(): JSX.Element {
-  // data layer
-  const authRepository = new AuthFakeApi();
-  // domain layer
-  const authHolder = new AuthHolder();
-  const loginUseCase = new LoginUseCase(authRepository, authHolder);
-  // view layer
-  const authViewModel = new AuthViewModelImpl(loginUseCase, authHolder);
+    // data layer
+    const authRepository = new AuthFakeApi();
+    // domain layer
+    const authHolder = new AuthHolder();
+    const loginUseCase = new LoginUseCase(authRepository, authHolder);
+    // view layer
+    const authViewModel = new AuthViewModelImpl(loginUseCase, authHolder);
 
-  return (
-      <div className="app-container d-flex container-fluid">
-        <AuthComponent authViewModel={authViewModel} />
-      </div>
-  );
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/login" exact={true} render={() => {
+                    return (<div className="app-container d-flex container-fluid">
+                        <AuthComponent authViewModel={authViewModel}/>
+                    </div>)
+                }}/>
+                <Route path="/" render={() => {
+                    return <h1>kekmda route</h1>
+                }}/>
+                <Route render={() => {
+                    return <h1>Not found route!</h1>
+                }}/>
+            </Switch>
+        </BrowserRouter>
+
+    );
 }
 
 export default App;
