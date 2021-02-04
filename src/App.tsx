980431abd96dffe4dfe5a8add72c0d64c6255be7
Deiklov <romanov408g@mail.ru>
2021-02-04 19:36:20 +0300

    base view for profile and signup and dockerignore

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import AuthViewModelImpl from './presentation/view-model/auth/AuthViewModelImpl'
 import AuthFakeApi from './data/auth/AuthFakeApi';
 import LoginUseCase from './domain/interactors/auth/LoginUseCase';
 import AuthHolder from './domain/entity/auth/models/AuthHolder';
+import SignupComponent from "./presentation/view/auth/SignUp";
+import ProfileComponent from "./presentation/view/profile/Profile";
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 
 function App(): JSX.Element {
@@ -24,12 +26,14 @@ function App(): JSX.Element {
                         <AuthComponent authViewModel={authViewModel}/>
                     </div>)
                 }}/>
-                <Route path="/" render={() => {
+                <Route path="/" exact={true} render={() => {
                     return <h1>kekmda route</h1>
                 }}/>
-                <Route render={() => {
-                    return <h1>Not found route!</h1>
+                <Route path="/stocks/:companyID" render={() => {
+                    return <h1>Apple stocks</h1>
                 }}/>
+                <Route path="/signup" component={SignupComponent}/>
+                <Route path="/profile" component={ProfileComponent}/>
             </Switch>
         </BrowserRouter>
 
