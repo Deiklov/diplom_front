e18d6d6742dfe20cd3e9c382a1fe074480e948dc
Deiklov <romanov408g@mail.ru>
2021-03-03 14:36:38 +0300

    with mobx

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,53 @@
 import React, {Component} from "react";
 import './App.css';
 import {BrowserRouter, Switch, Route, Link, Redirect} from 'react-router-dom'
+import {withRouter} from "react-router";
 import "antd/dist/antd.css";
 import {Layout, Menu, Breadcrumb} from 'antd';
-import LoginContainer from "./containers/loginPage";
-import SignUpContainer from "./containers/SignupPage";
-import ProfilePage from "./containers/profilePage";
+import LoginPage from "./pages/loginPage";
+import SignUpContainer from "./pages/signupPage";
+import ProfilePage from "./pages/profilePage";
 import {Row, Col} from 'antd';
+import {inject, observer} from "mobx-react";
 
 const {Header, Content, Footer} = Layout;
 
-export default class App extends Component {
-    isLoggedIn = false;
+class App extends Component {
+    componentWillMount() {
+        if (!this.props.commonStore.token) {
+            this.props.commonStore.setAppLoaded();
+        }
+    }
+
+    componentDidMount() {
+        // if (this.props.commonStore.token) {
+        //     this.props.userStore
+        //         .pullUser()
+        //         .finally(() => this.props.commonStore.setAppLoaded());
+        // }
+    }
 
     render() {
-        return (
-            <Layout className="layout">
-                <BrowserRouter>
+        if (this.props.commonStore.appLoaded) {
+            return (
+                <div>
                     <Header style={{position: "sticky", top: "0"}}>
                         <div className="logo"/>
                         <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
                             <Menu.Item key="1"><Link to="/login">login</Link></Menu.Item>
                             <Menu.Item key="2"><Link to="/signup">signup</Link></Menu.Item>
                             <Menu.Item key="3"><Link to="/profile">profile</Link></Menu.Item>
+                            
                         </Menu>
                     </Header>
-                    <Content style={{padding: '0 50px'}}>
                         <Switch>
                             <Route path="/login" exact>
                                 <Row>
-                                    <Col span={8} offset={8}><LoginContainer/></Col>
+                                    <Col span={8} offset={8}><LoginPage/></Col>
                                 </Row>
                             </Route>
                             <Route path="/" exact>
-                                <Redirect to="/login"/>
+                                <Redirect to="/profile"/>
                             </Route>
                             <Route path="/signup" exact>
                                 <Row>
@@ -46,11 +60,23 @@ export default class App extends Component {
                                 </Row>
                             </Route>
                         </Switch>
-                    </Content>
+                    <h1>token :{this.props.commonStore.token}</h1>
                     <Footer style={{textAlign: "center"}}>Ant Design ©2018 Created by BMSTU</Footer>
-                </BrowserRouter>
-            </Layout>
-        );
+                </div>
+            );
+        }
+        return (
+            <Header style={{position: "sticky", top: "0"}}>
+                <div className="logo"/>
+                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+                    <Menu.Item key="1"><Link to="/login">loginlow</Link></Menu.Item>
+                    <Menu.Item key="2"><Link to="/signup">signup</Link></Menu.Item>
+                    <Menu.Item key="3"><Link to="/profile">profile</Link></Menu.Item>
+                </Menu>
+            </Header>
+        )
     }
 
 }
+
+export default inject("authStore", "commonStore")(withRouter(observer(App)));
---
+++ src/store/commonStore.js
@@ -0,0 +1,37 @@
+import {observable, action, reaction, makeObservable} from 'mobx';
+
+class CommonStore {
+
+    appName = 'Romanov Invest';
+    token = window.localStorage.getItem('jwt');
+
+    constructor() {
+        reaction(
+            () => this.token,
+            token => {
+                if (token) {
+                    window.localStorage.setItem('jwt', token);
+                } else {
+                    window.localStorage.removeItem('jwt');
+                }
+            }
+        );
+        makeObservable(this, {
+            appName: observable,
+            token: observable,
+            setToken: action,
+            setAppLoaded: action,
+        })
+    }
+
+    setToken(token) {
+        this.token = token;
+    }
+
+    setAppLoaded() {
+        this.appLoaded = true;
+    }
+
+}
+
+export default new CommonStore();
